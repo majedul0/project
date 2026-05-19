e4c992e7580219506d4e3a0b1f3ede8e06571b9a
majedul0 <islam22205101253@diu.edu.bd>
2026-05-19 15:30:08 +0600

    up

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,9 @@ import './Footer.css';
 const Footer = ({ withSideNav = false }) => {
   return (
     <footer className={`footer ${withSideNav ? 'footer--with-sidenav' : ''}`}>
+      <div className="footer__scanline" aria-hidden="true" />
       <div className="footer__container">
-        <div className="footer__logo-section">
+        <div className="footer__logo-section footer__fade footer__fade--1">
           <img
             src="https://lh3.googleusercontent.com/aida-public/AB6AXuCHVLDf3R9E9LZHA5t-YZnmvnMbapn114Sv9TFs3HnOPbFRBfxwDyEQn58Mei5jgzfkQx8G6azdnei9Vc9yJWSYS029GIC9H2vEaIBY93MNNoM5jzbgamVbfCRz0Yvw_eGpbmKoI8C0YcrheNnNE_ZUoD2XYtyQ1d60W1-JwHTAx0bauX1WRrikqPBg90StXlBExPkt7QdDETyAyjyOaVi0gkavS9-q1Yw6Lct5XOISAvO-YR6pY6XpkAlVB2_Ztcr1Tlt2sIpmVE0"
             alt="IT Servicer Logo"
@@ -14,13 +15,13 @@ const Footer = ({ withSideNav = false }) => {
           <span className="footer__logo-text">IT SERVICER</span>
         </div>
         
-        <div className="footer__links">
+        <div className="footer__links footer__fade footer__fade--2">
           <a href="#" className="footer__link">Terms</a>
           <a href="#" className="footer__link">Privacy</a>
           <a href="#" className="footer__link">Support</a>
         </div>
         
-        <div className="footer__copyright">
+        <div className="footer__copyright footer__fade footer__fade--3">
           © 2024 IT SERVICER // NEURAL PROTOCOL
         </div>
       </div>
