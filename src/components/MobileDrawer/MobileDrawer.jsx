19957d0a119ee24a0993428339c792b4d6971576
majedul0 <islam22205101253@diu.edu.bd>
2026-05-25 12:44:00 +0600

    up

--- src/components/MobileDrawer/MobileDrawer.jsx
+++ src/components/MobileDrawer/MobileDrawer.jsx
@@ -1,46 +1,48 @@
-import React, { useState } from 'react';
-import './MobileDrawer.css';
+import { NavLink } from 'react-router-dom'
+import './MobileDrawer.css'
 
 const MobileDrawer = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+  if (!isOpen) return null
 
   return (
     <div className="mobile-drawer">
       <div className="mobile-drawer__header">
         <span className="mobile-drawer__title">SYSTEM MENU</span>
-        <button className="mobile-drawer__close-btn" onClick={onClose}>
+        <button type="button" className="mobile-drawer__close-btn" onClick={onClose} aria-label="Close menu">
           <span className="material-symbols-outlined">close</span>
         </button>
       </div>
-      
+
       <nav className="mobile-drawer__nav">
-        <a href="#" className="mobile-drawer__nav-item mobile-drawer__nav-item--active">
-          <span className="material-symbols-outlined">grid_view</span>
-          <span>System</span>
-        </a>
-        <a href="#services" className="mobile-drawer__nav-item" onClick={onClose}>
+        <NavLink to="/" end className="mobile-drawer__nav-item" onClick={onClose}>
+          <span className="material-symbols-outlined">home</span>
+          <span>Home</span>
+        </NavLink>
+        <NavLink to="/services" className="mobile-drawer__nav-item" onClick={onClose}>
           <span className="material-symbols-outlined">hub</span>
-          <span>Modules</span>
-        </a>
-        <a href="#stack" className="mobile-drawer__nav-item" onClick={onClose}>
+          <span>Services</span>
+        </NavLink>
+        <NavLink to="/blog" className="mobile-drawer__nav-item" onClick={onClose}>
+          <span className="material-symbols-outlined">article</span>
+          <span>Blog</span>
+        </NavLink>
+        <a href="/#stack" className="mobile-drawer__nav-item" onClick={onClose}>
           <span className="material-symbols-outlined">layers</span>
           <span>Tech Stack</span>
         </a>
-        <a href="#" className="mobile-drawer__nav-item">
-          <span className="material-symbols-outlined">sensors</span>
-          <span>Uplink</span>
-        </a>
       </nav>
-      
+
       <div className="mobile-drawer__status">
         <div className="mobile-drawer__status-box">
-          SYSTEM STATUS: OPTIMAL<br />
-          ENCRYPTION: AES-256<br />
+          SYSTEM STATUS: OPTIMAL
+          <br />
+          ENCRYPTION: AES-256
+          <br />
           UPLINK: ACTIVE
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default MobileDrawer;
+export default MobileDrawer
