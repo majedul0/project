e54abc166ae31d6c13528ca261f4232b17e8d5b6
majedul0 <islam22205101253@diu.edu.bd>
2026-05-25 12:44:13 +0600

    up

--- src/components/TopAppBar/TopAppBar.jsx
+++ src/components/TopAppBar/TopAppBar.jsx
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
-import './TopAppBar.css';
-import logoImg from '../../assets/logo/it-servicer logo bg remov.png';
+import { NavLink } from 'react-router-dom'
+import './TopAppBar.css'
+import logoImg from '../../assets/logo/it-servicer logo bg remov.png'
 
 const TopAppBar = ({ onMenuClick, showNavLinks = true }) => {
   return (
     <header className="top-app-bar">
       <div className="top-app-bar__left">
-        <button className="top-app-bar__menu-btn" onClick={onMenuClick}>
+        <button type="button" className="top-app-bar__menu-btn" onClick={onMenuClick} aria-label="Open menu">
           <span className="material-symbols-outlined">menu</span>
         </button>
-        <div className="top-app-bar__logo">
-          <img
-            src={logoImg}
-            alt="IT Servicer Logo"
-            className="top-app-bar__logo-img"
-          />
-        </div>
+        <NavLink to="/" className="top-app-bar__logo">
+          <img src={logoImg} alt="IT Servicer Logo" className="top-app-bar__logo-img" />
+        </NavLink>
       </div>
-      
+
       {showNavLinks && (
         <nav className="top-app-bar__nav">
-          <a href="#" className="top-app-bar__nav-link top-app-bar__nav-link--active">System</a>
-          <a href="#services" className="top-app-bar__nav-link">Modules</a>
-          <a href="#stack" className="top-app-bar__nav-link">Stack</a>
-          <a href="#" className="top-app-bar__nav-link">Uplink</a>
+          <NavLink to="/" end className="top-app-bar__nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/services" className="top-app-bar__nav-link">
+            Services
+          </NavLink>
+          <NavLink to="/blog" className="top-app-bar__nav-link">
+            Blog
+          </NavLink>
+          <a href="/#stack" className="top-app-bar__nav-link">
+            Stack
+          </a>
         </nav>
       )}
-      
+
       <div className="top-app-bar__right">
         <span className="material-symbols-outlined top-app-bar__icon">sensors</span>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default TopAppBar;
+export default TopAppBar
